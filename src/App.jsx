e02ae28c4b3823d94ac87160db3c74cc8f0767eb
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-12-24 16:05:28 +0600

    add smooth scroll plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
 import NavBar from './components/NavBar';
 import HeroSection from './sections/HeroSection';
-import { ScrollTrigger } from 'gsap/all';
+import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
 import gsap from 'gsap';
 import MessageSection from './sections/MessageSection';
 import FlavorSection from './sections/FlavorSection';
+import { useGSAP } from '@gsap/react';
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    })
+  })
   return (
     <main>
       <NavBar/>
+      <div id="smooth-wrapper">
+		  <div id="smooth-content">
       <HeroSection/>
       <MessageSection/>
       <FlavorSection/>
-      <div className="h-dvh border border-red-500"></div>
+      <div className="h-dvh border border-red-500"/>
+      </div>
+      </div>
     </main>
   )
 }
--- src/components/FlavorSlider.jsx
+++ src/components/FlavorSlider.jsx
@@ -2,27 +2,55 @@ import React, { useRef } from 'react'
 import { flavorlists } from '../constants'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import { useMediaQuery } from 'react-responsive'
 
 const FlavorSlider = () => {
     const sliderRef = useRef();
+
+    const isTablet = useMediaQuery({
+        query: '(max-width: 1024px)',
+    })
+
     useGSAP(() => {
         const scrollAmount = sliderRef.current.scrollWidth - window.innerWidth;
         
-        
-        const tl = gsap.timeline({
+        if(!isTablet){
+            const tl = gsap.timeline({
             scrollTrigger: {
                 trigger: '.flavor-section',
                 start: '2% top',
                 end: `+=${scrollAmount + 1500}px`,
                 scrub: true,
                 pin: true,
-                markers: true
+                }
+            })
+            tl.to('.flavor-section', {
+                x: `-${scrollAmount + 1500}px`,
+                ease: 'power1.inOut'
+            })
+        }
+        
+        
+
+        const titleTl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.flavor-section',
+                start: 'top top',
+                end: 'bottom 80%',
+                scrub: true
             }
-        })
-        tl.to('.flavor-section', {
-            x: `-${scrollAmount + 1500}px`,
+        });
+
+        titleTl.to('.first-text-split', {
+            xPercent: -30,
             ease: 'power1.inOut'
-        })
+        }).to('.flavor-text-scroll', {
+            xPercent: -22,
+            ease: 'power1.inOut'
+        }, '<').to('.second-text-split', {
+            xPercent: -10,
+            ease: 'power1.inOut'
+        }, '<')
     })
   return (
     <div ref={sliderRef} className='slider-wrapper'>
